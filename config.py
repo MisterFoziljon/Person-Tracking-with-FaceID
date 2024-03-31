e670150831efd97cea41222d05405c8a182994d5
Foziljon <127921695+MisterFoziljon@users.noreply.github.com>
2024-03-31 22:12:05 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,94 @@
+import argparse
+from vision import Point 
+from shapely.geometry.polygon import Polygon
+
+def parse_args():
+
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    
+    parser.add_argument(
+        "--face_detector", 
+        type=str, 
+        default='/home/foziljon/PROJECTS/ATTENDENCE_SYSTEM/models/det_500m.onnx', 
+        help="The path of the facial detection model")
+    
+    parser.add_argument(
+        "--face_recognizer",
+        type=str, 
+        default='/home/foziljon/PROJECTS/ATTENDENCE_SYSTEM/models/w600k_mbf.onnx',
+        help="The path of the facial recognition model")
+
+    parser.add_argument(
+        "--person_detector",
+        type=str, 
+        default='/home/foziljon/PROJECTS/ATTENDENCE_SYSTEM/models/yolov8n.pt',
+        help="The path of the facial recognition model")
+    
+    parser.add_argument(
+        "--in_rtsp",
+        type=str,
+        default='sources/in.mp4',
+        help="video source path or rtsp protocol")
+
+    parser.add_argument(
+        "--out_rtsp",
+        type=str,
+        default='sources/out.mp4',
+        help="video source path or rtsp protocol")
+    
+    parser.add_argument(
+        "--use_cpu",
+        type=bool, 
+        default=False)
+    
+    parser.add_argument(
+        "--database_folder", 
+        type=str, 
+        default="/home/foziljon/PROJECTS/ATTENDENCE_SYSTEM/dataset")
+    
+    parser.add_argument(
+        "--in_line_xy_min", 
+        type=Point,
+        default=Point(910,700))
+
+    parser.add_argument(
+        "--in_line_xy_max", 
+        type=Point,
+        default=Point(2240,700))
+    
+    parser.add_argument(
+        "--out_line_xy_min", 
+        type=Point,
+        default=Point(2880,890))
+
+    parser.add_argument(
+        "--out_line_xy_max", 
+        type=Point,
+        default=Point(2340,1630))
+
+    parser.add_argument(
+        "--polygon", 
+        type=Polygon,
+        default=Polygon([(770,890),(1610,530),(3270,1010),(2790,1860)]))
+
+    parser.add_argument(
+        "--face_threshold",
+        type=float,
+        default=0.4)
+
+    parser.add_argument(
+        "--person_threshold",
+        type=float,
+        default=0.5)
+
+    args = parser.parse_args()
+
+    return args
+
+def main():
+    FLAGS = parse_args()
+    print(FLAGS.polygon)
+
+
+if __name__ == '__main__':
+    main()
